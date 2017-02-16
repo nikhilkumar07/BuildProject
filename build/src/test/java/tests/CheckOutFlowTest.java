77a2b1b0f505071c9bac0d32fa9badc71b5c2e99
nikhilkumar07 <sayi.niikil4@gmail.com>
2017-02-17 02:23:23 +0530

    Update CheckOutFlowTest.java

--- build/src/test/java/tests/CheckOutFlowTest.java
+++ build/src/test/java/tests/CheckOutFlowTest.java
@@ -31,13 +31,12 @@ public class CheckOutFlowTest {
 		// filling shipping and card details if the user not saved any information shipping information
 		ReviewOrder.FillingShippingAddress( "firstname","String lastname","companyname","String address1","String address2","32423","24234243234" );
 		/*
-		Many Florida citizens are not aware that this Florida has a "use tax." Use tax normally applies to items purchased outside Florida which are brought or delivered into the state and would have been taxed if purchased in Florida. This includes purchases made over the internet. The use tax rate is the same as the sales tax rate, 6%. Build.com is an out-of-state seller so it is your responsibility to comply with Florida law. You must submit payment directly to the Florida Department of Revenue. This payment is required by section 212.06(8), Florida Statutes. You can file and pay use tax online or complete an Out-of-State Purchase Return (Form DR-15MO).
-		*/
-		float tax = (float) 0;
+		Build.com, Inc. is located in California and we are obligated to collect California Sales Tax on any items shipped to destinations in California. Sales Tax is collected for all orders shipping to California unless a valid resale certificate is presented prior to purchase. The current sales tax rate is 7.25%.*/
+		float tax = ;
 		
 		// asserting grand tax and grand total.
 		// in Florida the good of florida doesnot have any tax
-		Assert.assertEquals(homepage.tax(), tax);
+		Assert.assertEquals(homepage.tax(), homepage.CAtax());
 		
 		Assert.assertEquals(homepage.totalActual(), homepage.totalExpected());
 		
