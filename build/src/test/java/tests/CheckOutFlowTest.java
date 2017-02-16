d6298324ed7556d9f2a1be75a3cc0f3faf4d737c
nikhilkumar07 <sayi.niikil4@gmail.com>
2017-02-17 01:53:55 +0530

    Update CheckOutFlowTest.java

--- build/src/test/java/tests/CheckOutFlowTest.java
+++ build/src/test/java/tests/CheckOutFlowTest.java
@@ -28,7 +28,7 @@ public class CheckOutFlowTest {
 		homepage.enterSearchDataAndAddItToCart("Kohler K-5180-ST","2");
 		//homepage.enterSearchDataAndAddItToCart("Kohler K-5180-ST");
 		homepage.clickOnCartBtn();
-		// filling shipping and card details if the user not given any information
+		// filling shipping and card details if the user not saved any information shipping information
 		ReviewOrder.FillingShippingAddress( "firstname","String lastname","companyname","String address1","String address2","32423","24234243234" );
 		/*
 		Many Florida citizens are not aware that this Florida has a "use tax." Use tax normally applies to items purchased outside Florida which are brought or delivered into the state and would have been taxed if purchased in Florida. This includes purchases made over the internet. The use tax rate is the same as the sales tax rate, 6%. Build.com is an out-of-state seller so it is your responsibility to comply with Florida law. You must submit payment directly to the Florida Department of Revenue. This payment is required by section 212.06(8), Florida Statutes. You can file and pay use tax online or complete an Out-of-State Purchase Return (Form DR-15MO).
