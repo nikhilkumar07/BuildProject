f439271688024586dbefc10edc4e82ceaf513e2c
nikhilkumar07 <sayi.niikil4@gmail.com>
2017-02-17 01:47:49 +0530

    Update CheckOutFlowTest.java

--- build/src/test/java/tests/CheckOutFlowTest.java
+++ build/src/test/java/tests/CheckOutFlowTest.java
@@ -1,21 +1,39 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import common.WebTest;
 import pages.HomePage;
+import pages.Payments;
 
-public class CheckOutFlowTest extends WebTest {
-	
+public class CheckOutFlowTest {
+
+	HomePage homepage = new HomePage();
+	Payments ReviewOrder = new Payments();
 	
 	@Test
-	public void verifyCheckOutFlow() throws InterruptedException{
-		HomePage homepage = new HomePage(getDriver());
+	public void verifyCheckOutFlow(){
+	
 		homepage.navigateToUrl();
-		homepage.enterSearchDataAndAddItToCart("Suede Kohler K66266U");
-		homepage.enterSearchDataWithQuantityAddItToCart("Kohler K-5180-ST");
-		homepage.enterSearchCashmereIteamAndAddItToCart("Cashmere Kohler K66266U");
+		homepage.enterSearchDataAndAddItToCart("Suede Kohler K66266U","1");
+		
+		homepage.enterSearchDataAndAddItToCart("Cashmere Kohler K66266U","1");
+		
+		
+		homepage.enterSearchDataAndAddItToCart("Kohler K-5180-ST","2");
+		//homepage.enterSearchDataAndAddItToCart("Kohler K-5180-ST");
 		homepage.clickOnCartBtn();
+		
+		ReviewOrder.FillingShippingAddress( "firstname","String lastname","companyname","String address1","String address2","32423","24234243234" );
+		
+		float tax = (float) 0;
+		
+		
+		// in Florida the good of florida doesnot have any tax
+		Assert.assertEquals(homepage.tax(), tax);
+		
+		Assert.assertEquals(homepage.totalActual(), homepage.totalExpected());
+		
 	}
 	
 }
